35da9e59e715a6631b6514e2255be04c2ea25927
sankukai7126 <sankukaix@gmail.com>
2020-12-09 16:56:31 +0100

    Test Classe Entiere

--- src/test/java/cfranc/hanoi/DisqueTest.java
+++ src/test/java/cfranc/hanoi/DisqueTest.java
@@ -17,7 +17,14 @@ public class DisqueTest {
 		Disque grand = new Disque(3);
 		assertEquals(-1, petit.compareTo(moyen));
 		assertEquals(-1, moyen.compareTo(grand));
+		assertEquals(-1, petit.compareTo(grand));
 		//TODO : à compléter
+		assertEquals(1, grand.compareTo(moyen));
+		assertEquals(1, moyen.compareTo(petit));
+		assertEquals(1, grand.compareTo(petit));
+		assertEquals(0, petit.compareTo(petit));
+		assertEquals(0, moyen.compareTo(moyen));
+		assertEquals(0, grand.compareTo(grand));
 	}
 
 	@Test
--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -6,9 +6,38 @@ import org.junit.Test;
 
 public class TourTest {
 
+	
+	Disque d1 = new Disque(1);
+	Disque d3 = new Disque(3);
+	
+	
 	@Test
-	public void test() {
-
+	/**
+	 * Given_When_Then 
+	*/
+	public void testEmpiler_TourNonVideEtDisquePlusPetitVrai() {
+		
+		Tour _TourNonVide= new Tour(2);
+		_TourNonVide.empiler(d3);
+		
+		boolean expected = true;
+		boolean actual = _TourNonVide.empiler(d1);
+		
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testEmpiler_TourNonVideEtDisquePlusGrandFaux() {
+				
+		Tour _TourNonVide = new Tour(2);
+		_TourNonVide.empiler(d1);
+		
+		boolean expected = false;
+		boolean actual = _TourNonVide.empiler(d3);
+		
+		assertEquals(expected, actual);
+		
 	}
 
 }
